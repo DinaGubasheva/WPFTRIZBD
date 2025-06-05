da9da648ff84a026f64b1eaf9ddad71b5416d9a7
DinaGubasheva <gubasheva.dina@mail.ru>
2025-06-06 02:59:33 +0300

    Create Result.cs

---
+++ Result.cs
@@ -0,0 +1,58 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace rTRIZBD4
+{
+    public class Result : INotifyPropertyChanged
+    {
+        private string _fullName;
+        private string _score;
+        private string _location;
+
+        public string FullName
+        {
+            get => _fullName;
+            set
+            {
+                if (_fullName != value)
+                {
+                    _fullName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Score
+        {
+            get => _score;
+            set
+            {
+                if (_score != value)
+                {
+                    _score = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Location
+        {
+            get => _location;
+            set
+            {
+                if (_location != value)
+                {
+                    _location = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
